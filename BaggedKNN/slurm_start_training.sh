0790efadf694b2c63fda14e2f095f18e3132ac55
Toivancenobi <tobias.ettling@gmx.de>
2023-08-24 15:47:35 +0200

    shap

--- BaggedKNN/slurm_start_training.sh
+++ BaggedKNN/slurm_start_training.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=3000
+#SBATCH --mem-per-cpu=8000
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --output out/output_BKNN.txt
