546f458ce8586f8b7b48079b049e1ae5f7a5d8ea
Toivancenobi <tobias.ettling@gmx.de>
2023-08-25 13:57:10 +0200

    BKNN new params

--- BaggedKNN/training.py
+++ BaggedKNN/training.py
@@ -40,7 +40,7 @@ for ts in training_sets:
         }
 
         # Create a KNN Regressor
-        knn_regressor = KNeighborsRegressor(n_jobs=5)
+        knn_regressor = KNeighborsRegressor(n_jobs=30)
 
         # Create a Bagging KNN Regressor
         model = BaggingRegressor(estimator=knn_regressor, random_state=42)
