1f051d71176f010c24bcc321462fe61fcf064501
Toivancenobi <tobias.ettling@gmx.de>
2023-08-26 12:22:11 +0200

    LogR and MLP adjust for TS4

--- MLP/slurm_start_training.sh
+++ MLP/slurm_start_training.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=MLP
-#SBATCH --partition=general2
-#SBATCH --nodes=4
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=150
+#SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=3000
 #SBATCH --time=20:00:00
 #SBATCH --mail-type=FAIL
