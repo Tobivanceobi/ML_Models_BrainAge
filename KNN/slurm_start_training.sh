edaca217bf86310fdcd150335a10d497d8e1c678
Toivancenobi <tobias.ettling@gmx.de>
2023-08-29 11:53:38 +0200

    KNN new Param

--- KNN/slurm_start_training.sh
+++ KNN/slurm_start_training.sh
@@ -6,6 +6,7 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=1000
 #SBATCH --time=10:00:00
+#SBATCH --array=0-2
 #SBATCH --mail-type=FAIL
 #SBATCH --output out/output_KNN.txt
 #SBATCH --error err/error_KNN.txt
@@ -16,6 +17,6 @@
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda init bash
 conda activate pytorch
-python3 $HOME/tobias_ettling/ML_Models_BrainAge/KNN/training.py
+python3 $HOME/tobias_ettling/ML_Models_BrainAge/KNN/training.py $SLURM_ARRAY_TASK_ID
 
 conda deactivate
--- KNN/training.py
+++ KNN/training.py
@@ -11,44 +11,46 @@ import pandas as pd
 from skopt import BayesSearchCV
 
 
+pid = int(sys.argv[1])
+
 training_sets = ['TS2/', 'TS4/']
 set_vary = ['meanEpochs/', 'meanEpochs/onlyEC/', 'meanEpochs/onlyEO/']
 for ts in training_sets:
-    for sv in set_vary:
-        set_path = SET_PATH + ts + sv
-        data = load_object(set_path + 'training_set')
-        x = data['x']
-        groups = data['group']
-        y = data['y']
+    sv = set_vary[pid]
+    set_path = SET_PATH + ts + sv
+    data = load_object(set_path + 'training_set')
+    x = data['x']
+    groups = data['group']
+    y = data['y']
 
-        y_skf = [int(age) for age in data['y']]
-        skf_vals = []
-        skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
-        for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
-            skf_vals.append((train_index, test_index))
+    y_skf = [int(age) for age in data['y']]
+    skf_vals = []
+    skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
+    for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
+        skf_vals.append((train_index, test_index))
 
-        scaler = StandardScaler()
-        x = scaler.fit_transform(x)
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
 
-        parameter_space = {
-            'n_neighbors': [3, 50],
-            'leaf_size': [3, 100],
-            'p': [1, 2]
-        }
+    parameter_space = {
+        'n_neighbors': [3, 50],
+        'leaf_size': [3, 100],
+        'p': [1, 2]
+    }
 
-        model = KNeighborsRegressor(n_jobs=-2)
+    model = KNeighborsRegressor(n_jobs=-2)
 
-        clf = BayesSearchCV(estimator=model,
-                            search_spaces=parameter_space,
-                            cv=skf_vals,
-                            scoring='neg_mean_absolute_error',
-                            verbose=4)
+    clf = BayesSearchCV(estimator=model,
+                        search_spaces=parameter_space,
+                        cv=skf_vals,
+                        scoring='neg_mean_absolute_error',
+                        verbose=4)
 
-        clf.fit(x, y=y)
+    clf.fit(x, y=y)
 
-        print(clf.cv_results_)
-        print(clf.best_score_)
-        print(clf.best_params_)
-        results = pd.DataFrame(clf.cv_results_)
-        f_name = ts.replace('/', '_') + sv.replace('/', '_')
-        results.to_csv(BASE_PATH + f'KNN/{f_name}results.csv')
+    print(clf.cv_results_)
+    print(clf.best_score_)
+    print(clf.best_params_)
+    results = pd.DataFrame(clf.cv_results_)
+    f_name = ts.replace('/', '_') + sv.replace('/', '_')
+    results.to_csv(BASE_PATH + f'KNN/{f_name}results.csv')
