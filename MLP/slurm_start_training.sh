95054a6d38f62e279669a0ab3083a29bc7f57a9d
Toivancenobi <tobias.ettling@gmx.de>
2023-08-29 12:41:54 +0200

    new param for MLP

--- MLP/slurm_start_training.sh
+++ MLP/slurm_start_training.sh
@@ -5,10 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=3000
+#SBATCH --array=0-2
 #SBATCH --time=20:00:00
 #SBATCH --mail-type=FAIL
-#SBATCH --output out/output_MLP.txt
-#SBATCH --error err/error_MLP.txt
+#SBATCH --output out/output_MLP_%a.txt
+#SBATCH --error err/error_MLP_%a.txt
 
 # Remove previous results
 # rm err/*; rm out/*; rm -r runs/*;
@@ -16,6 +17,6 @@
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda init bash
 conda activate pytorch
-python3 $HOME/tobias_ettling/ML_Models_BrainAge/MLP/training.py
+python3 $HOME/tobias_ettling/ML_Models_BrainAge/MLP/training.py $SLURM_ARRAY_TASK_ID
 
 conda deactivate
--- MLP/training.py
+++ MLP/training.py
@@ -12,13 +12,13 @@ from sklearn.preprocessing import MinMaxScaler
 from config import SET_PATH, BASE_PATH
 from helper import load_object
 
+pid = int(sys.argv[1])
+
 
 class MLPWrapper(BaseEstimator, RegressorMixin):
     def __init__(self,
                  layer1=None,
                  layer2=None,
-                 layer3=None,
-                 layer4=None,
                  num_hl=None,
                  activation=None,
                  solver=None,
@@ -27,8 +27,6 @@ class MLPWrapper(BaseEstimator, RegressorMixin):
                  alpha=None):
         self.layer1 = layer1
         self.layer2 = layer2
-        self.layer3 = layer3
-        self.layer4 = layer4
         self.num_hl = num_hl
         self.activation = activation
         self.solver = solver
@@ -37,14 +35,14 @@ class MLPWrapper(BaseEstimator, RegressorMixin):
         self.alpha = alpha
 
     def fit(self, x_train, y_train):
-        print([self.layer1, self.layer2, self.layer3, self.layer4][-1*self.num_hl:])
+        print([self.layer1, self.layer2][-1*self.num_hl:])
         print(self.solver)
         print(self.learning_rate_init)
         print(self.activation)
         print(self.alpha)
         model_mlp = MLPRegressor(
-            hidden_layer_sizes=[self.layer1, self.layer2, self.layer3, self.layer4][-1*self.num_hl:],
-            max_iter=400,
+            hidden_layer_sizes=[self.layer1, self.layer2][-1*self.num_hl:],
+            max_iter=300,
             activation=self.activation,
             solver=self.solver,
             learning_rate=self.learning_rate,
@@ -62,52 +60,51 @@ class MLPWrapper(BaseEstimator, RegressorMixin):
         return self.model.score(x_train, y_train)
 
 
-training_sets = ['TS2/']
+training_sets = ['TS2/', 'TS4/']
 set_vary = ['meanEpochs/', 'meanEpochs/onlyEC/', 'meanEpochs/onlyEO/']
 for ts in training_sets:
-    for sv in set_vary:
-        set_path = SET_PATH + ts + sv
-        data = load_object(set_path + 'training_set')
-        x = data['x']
-        groups = data['group']
-        y = data['y']
+    sv = set_vary[pid]
+    set_path = SET_PATH + ts + sv
+    data = load_object(set_path + 'training_set')
+    x = data['x']
+    groups = data['group']
+    y = data['y']
 
-        y_skf = [int(age) for age in data['y']]
-        skf_vals = []
-        skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
-        for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
-            skf_vals.append((train_index, test_index))
+    y_skf = [int(age) for age in data['y']]
+    skf_vals = []
+    skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
+    for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
+        skf_vals.append((train_index, test_index))
 
-        scaler = MinMaxScaler()
-        x = scaler.fit_transform(x)
+    scaler = MinMaxScaler()
+    x = scaler.fit_transform(x)
 
-        parameter_space = {
-            'layer1': Integer(5000, 10000),
-            'layer2': Integer(2000, 5000),
-            'layer3': Integer(1000, 2000),
-            'layer4': Integer(300, 1000),
-            'num_hl': Integer(1, 2),
-            'activation': Categorical(['tanh', 'relu']),
-            'solver': Categorical(['adam']),
-            'alpha': Real(0.0001, 0.05),
-            'learning_rate': Categorical(['constant', 'adaptive']),
-            'learning_rate_init': Real(0.0001, 0.1)
-        }
+    parameter_space = {
+        'layer1': Integer(1000, 2000),
+        'layer2': Integer(300, 1000),
+        'num_hl': Integer(1, 2),
+        'batch_size': Categorical([8, 32, 128, 256, 512]),
+        'activation': Categorical(['tanh']),
+        'solver': Categorical(['adam']),
+        'alpha': Real(0.00001, 0.0001),
+        'learning_rate': Categorical(['adaptive']),
+        'learning_rate_init': Real(0.0001, 0.1)
+    }
 
-        clf = BayesSearchCV(
-            estimator=MLPWrapper(),
-            search_spaces=parameter_space,
-            n_iter=30,
-            cv=skf_vals,
-            scoring='neg_mean_absolute_error',
-            n_jobs=30
-        )
+    clf = BayesSearchCV(
+        estimator=MLPWrapper(),
+        search_spaces=parameter_space,
+        n_iter=30,
+        cv=skf_vals,
+        scoring='neg_mean_absolute_error',
+        n_jobs=-3
+    )
 
-        clf.fit(x, y=y)
+    clf.fit(x, y=y)
 
-        print(clf.cv_results_)
-        print(clf.best_score_)
-        print(clf.best_params_)
-        results = pd.DataFrame(clf.cv_results_)
-        f_name = ts.replace('/', '_') + sv.replace('/', '_')
-        results.to_csv(BASE_PATH + f'MLP/{f_name}results.csv')
+    print(clf.cv_results_)
+    print(clf.best_score_)
+    print(clf.best_params_)
+    results = pd.DataFrame(clf.cv_results_)
+    f_name = ts.replace('/', '_') + sv.replace('/', '_')
+    results.to_csv(BASE_PATH + f'MLP/{f_name}results.csv')
