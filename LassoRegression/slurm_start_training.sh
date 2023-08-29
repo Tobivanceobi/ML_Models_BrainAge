e30507878d851aa5de6bad50ee73282d73280e9a
Toivancenobi <tobias.ettling@gmx.de>
2023-08-29 12:28:06 +0200

    new param for Lasso

--- LassoRegression/slurm_start_training.sh
+++ LassoRegression/slurm_start_training.sh
@@ -6,9 +6,10 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=1000
 #SBATCH --time=10:00:00
+#SBATCH --array=0-2
 #SBATCH --mail-type=FAIL
-#SBATCH --output out/output_Lasso.txt
-#SBATCH --error err/error_Lasso.txt
+#SBATCH --output out/output_Lasso_%a.txt
+#SBATCH --error err/error_Lasso_%a.txt
 
 # Remove previous results
 # rm err/*; rm out/*; rm -r runs/*;
@@ -16,6 +17,6 @@
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda init bash
 conda activate pytorch
-python3 $HOME/tobias_ettling/ML_Models_BrainAge/LassoRegression/training.py
+python3 $HOME/tobias_ettling/ML_Models_BrainAge/LassoRegression/training.py  $SLURM_ARRAY_TASK_ID
 
 conda deactivate
