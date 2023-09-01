52925add3379248737993417f2b576e98aff63dd
Toivancenobi <tobias.ettling@gmx.de>
2023-09-01 11:04:22 +0200

    RF paralized

--- RandomForrest/slurm_start_training.sh
+++ RandomForrest/slurm_start_training.sh
@@ -5,16 +5,17 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=2000
+#SBATCH --array=0-2
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=FAIL
-#SBATCH --output out/output_RF.txt
-#SBATCH --error err/error_RF.txt
+#SBATCH --output out/output_RF_%a.txt
+#SBATCH --error err/error_RF_%a.txt
 
 # Remove previous results
 # rm err/*; rm out/*; rm -r runs/*;
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda init bash
 conda activate pytorch
-python3 $HOME/tobias_ettling/ML_Models_BrainAge/RandomForrest/training.py
+python3 $HOME/tobias_ettling/ML_Models_BrainAge/RandomForrest/training.py $SLURM_ARRAY_TASK_ID
 
 conda deactivate
--- RandomForrest/training.py
+++ RandomForrest/training.py
@@ -9,55 +9,55 @@ from helper import load_object
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from skopt import BayesSearchCV
-
+pid = int(sys.argv[1])
 training_sets = ['TS2/']
 set_vary = ['meanEpochs/', 'meanEpochs/onlyEC/', 'meanEpochs/onlyEO/']
 for ts in training_sets:
-    for sv in set_vary:
-        set_path = SET_PATH + ts + sv
-        data = load_object(set_path + 'training_set')
-        x = data['x']
-        groups = data['group']
-        y_org = data['y']
-        y = [int(age * 10) for age in data['y']]
+    sv = set_vary[pid]
+    set_path = SET_PATH + ts + sv
+    data = load_object(set_path + 'training_set')
+    x = data['x']
+    groups = data['group']
+    y_org = data['y']
+    y = [int(age * 10) for age in data['y']]
 
-        le = LabelEncoder()
-        le.fit(y)
-        y = le.transform(y)
+    le = LabelEncoder()
+    le.fit(y)
+    y = le.transform(y)
 
-        y_skf = [int(age) for age in data['y']]
-        skf_vals = []
-        skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
-        for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
-            skf_vals.append((train_index, test_index))
+    y_skf = [int(age) for age in data['y']]
+    skf_vals = []
+    skf = StratifiedGroupKFold(n_splits=3, shuffle=True, random_state=126)
+    for fold, (train_index, test_index) in enumerate(skf.split(x, y_skf, groups)):
+        skf_vals.append((train_index, test_index))
 
-        parameter_space = {
-            'max_depth': [45, 100],
-            'min_samples_split': [2, 10],
-            'min_samples_leaf': [2, 9],
-            'min_weight_fraction_leaf': [0, 0.5],
-            'min_impurity_decrease': [0, 0.9],
-            'n_estimators': [30, 300]
-        }
+    parameter_space = {
+        'max_depth': [45, 100],
+        'min_samples_split': [2, 10],
+        'min_samples_leaf': [2, 9],
+        'min_weight_fraction_leaf': [0, 0.5],
+        'min_impurity_decrease': [0, 0.9],
+        'n_estimators': [30, 300]
+    }
 
-        model = RandomForestRegressor(n_jobs=-2)
+    model = RandomForestRegressor(n_jobs=-2)
 
-        fit_param = {
-            'early_stopping_rounds': 200,
-        }
+    fit_param = {
+        'early_stopping_rounds': 200,
+    }
 
-        clf = BayesSearchCV(estimator=model,
-                            search_spaces=parameter_space,
-                            fit_params=fit_param,
-                            cv=skf_vals,
-                            scoring='neg_mean_absolute_error',
-                            verbose=4)
+    clf = BayesSearchCV(estimator=model,
+                        search_spaces=parameter_space,
+                        fit_params=fit_param,
+                        cv=skf_vals,
+                        scoring='neg_mean_absolute_error',
+                        verbose=4)
 
-        clf.fit(x, y=y)
+    clf.fit(x, y=y)
 
-        print(clf.cv_results_)
-        print(clf.best_score_)
-        print(clf.best_params_)
-        results = pd.DataFrame(clf.cv_results_)
-        f_name = ts.replace('/', '_') + sv.replace('/', '_')
-        results.to_csv(BASE_PATH + f'RandomForrest/{f_name}results.csv')
+    print(clf.cv_results_)
+    print(clf.best_score_)
+    print(clf.best_params_)
+    results = pd.DataFrame(clf.cv_results_)
+    f_name = ts.replace('/', '_') + sv.replace('/', '_')
+    results.to_csv(BASE_PATH + f'RandomForrest/{f_name}results.csv')
