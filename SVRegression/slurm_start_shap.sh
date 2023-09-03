146f500f030b70f9b2c2452a1e0c290fbc2cf835
Toivancenobi <tobias.ettling@gmx.de>
2023-09-03 21:43:25 +0200

    svr

--- SVRegression/slurm_start_shap.sh
+++ SVRegression/slurm_start_shap.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=SVR_shap
+#SBATCH --extra-node-info=2:32:2
 #SBATCH --partition=gpu
 #SBATCH --nodes=4
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=128
 #SBATCH --mem-per-cpu=1000
 #SBATCH --time=15:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --output out/output_SVR_shap.txt
 #SBATCH --error err/error_SVR_shap.txt
-#SBATCH --extra-node-info=2:32:2
+
 # Remove previous results
 # rm err/*; rm out/*; rm -r runs/*;
 source $HOME/miniconda/etc/profile.d/conda.sh
